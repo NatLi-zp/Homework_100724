7abed84590ec0e1b118fb2cdfd0c64a697ffef7f
NatLi-zp <natali.gordienko@gmail.com>
2024-07-11 18:55:04 +0200

    Homework Java 3 (version 2) 110724

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String strStart = "";
+        String strEnd = "";
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter first String:");
         String str1 = scan.nextLine();
-        System.out.println("Length of first String:"+str1.length());
+        int lengthStr1 = str1.length();
+
+        if (lengthStr1 % 2 == 0) {
+            strStart = str1.substring(0, lengthStr1 / 2);
+        } else {
+            System.out.println("Error. Length of first String:" + str1.length());
+        }
 
         System.out.println("Enter second String:");
         String str2 = scan.nextLine();
-        System.out.println("Length of second String:"+str2.length());
+        int lengthStr2 = str2.length();
 
-        System.out.println(str1.substring(0,str1.length()/2)+str2.substring(str2.length()/2));
-          }
+        if (lengthStr2 % 2 == 0) {
+            strEnd = str2.substring(lengthStr2 / 2);
+        } else {
+            System.out.println("Error. Length of second String:" + str2.length());
+        }
+
+        if ((lengthStr1 % 2 == 0) && (lengthStr2 % 2 == 0)) {
+            System.out.println(strStart + strEnd);
+        } else {
+            System.out.println("Incorrect data entered!");
+        }
+    }
 }
